fcd15e4ea7827d36cabdf76fe47d8a74bc824de9
Umberto Valente <uvalente89@gmail.com>
2020-04-23 16:44:28 +0100

    week1: add feedback for card type or validity

--- week1/credit.c
+++ week1/credit.c
@@ -25,23 +25,47 @@ int main(void)
   }
 
   int checksum = 0;
-  for (int i = 1; i <= length; i += 2)
+  for (int i = 1; i < length; i += 2)
   {
-    numbers[i] *= 2;
-    if (numbers[i] < 10)
+    int number = numbers[i];
+    number *= 2;
+    if (number < 10)
     {
-      checksum += numbers[i];
+      checksum += number;
     }
     else
     {
-      checksum += numbers[i] % 10;
-      checksum += numbers[i] / 10;
+      checksum += number % 10;
+      checksum += number / 10;
     }
   }
 
-  for (int i = 0; i <= length; i += 2)
+  for (int i = 0; i < length; i += 2)
   {
     checksum += numbers[i];
   }
-  printf("checksum: %d\n", checksum);
+
+  if (checksum % 10 == 0 && length >= 13)
+  {
+    switch (numbers[length - 1])
+    {
+    case 3:
+      numbers[length - 2] == 4 || numbers[length - 2] == 7 ? printf("AMEX\n") : printf("INVALID\n");
+      break;
+    case 4:
+      printf("VISA\n");
+      break;
+    case 5:
+      numbers[length - 2] < 6 ? printf("MASTERCARD\n") : printf("INVALID\n");
+      break;
+    default:
+      printf("something wrong\n");
+      printf("check against %d\n", numbers[length - 1]);
+      break;
+    }
+  }
+  else
+  {
+    printf("INVALID\n");
+  }
 }
