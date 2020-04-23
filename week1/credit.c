ce9366c7d5324a646a709700c909e0ee9039c560
Umberto Valente <uvalente89@gmail.com>
2020-04-23 15:20:29 +0100

    week1: check length of number and allocate array size accordingly

--- week1/credit.c
+++ week1/credit.c
@@ -3,10 +3,19 @@
 int main(void)
 {
   long card_number;
-  int numbers[15];
   int pointer = 0;
+  int length = 0;
   printf("Number: ");
   scanf("%ld", &card_number);
+  long counter = card_number;
+  while (counter != 0)
+  {
+    counter /= 10;
+    length++;
+  }
+
+  int numbers[length];
+
   while (card_number > 0)
   {
     int digit = card_number % 10;
