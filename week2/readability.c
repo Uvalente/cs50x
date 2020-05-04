58ffeafc7427a46719aeda8ef14b5c296d69b10b
Umberto Valente <uvalente89@gmail.com>
2020-05-04 17:57:07 +0100

    week2: add count_sentences function

--- week2/readability.c
+++ week2/readability.c
@@ -7,6 +7,7 @@
 
 int count_letters(string string);
 int count_words(string string);
+int count_sentences(string string);
 
 int main(void)
 {
@@ -15,9 +16,24 @@ int main(void)
 
   int letters = count_letters(text);
   int words = count_words(text);
+  int sentences = count_sentences(text);
 
   printf("%d\n", letters);
   printf("%d\n", words);
+  printf("%d\n", sentences);
+
+  if (grade < 1)
+  {
+    printf("Before Grade 1\n");
+  }
+  else if (grade >= 16)
+  {
+    printf("Grade 16+\n");
+  }
+  else
+  {
+    printf("Grade #\n");
+  }
 }
 
 int count_words(string string)
@@ -50,4 +66,21 @@ int count_letters(string string)
   }
 
   return letters;
+}
+
+int count_sentences(string string)
+{
+  int sentences = 0;
+  int length = strlen(string);
+
+  for (int i = 0; i < length; i++)
+  {
+    int value = string[i];
+    if (value == 33 || value == 46 || value == 63)
+    {
+      sentences++;
+    }
+  }
+
+  return sentences;
 }
