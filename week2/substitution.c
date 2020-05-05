142cdb324570165eaaf5456e6ce0095f6221d28f
Umberto Valente <uvalente89@gmail.com>
2020-05-05 18:36:47 +0100

    week2: add case insensitive converting

--- week2/substitution.c
+++ week2/substitution.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(int argc, string argv[])
 {
@@ -24,20 +25,46 @@ int main(int argc, string argv[])
 
   for (int i = 0; i < 26; i++)
   {
-    values[i] = (int)argv[1][i];
+    values[i] = (int)toupper(argv[1][i]);
   }
 
   int text_size = strlen(plain_text);
 
   char cipher_text[text_size];
 
+  int letter_value;
   for (int i = 0; i < text_size; i++)
   {
-    for (int j = 65; j < 91; j++)
+
+    int is_lower = islower(plain_text[i]);
+    if (is_lower)
     {
-      if ((int)plain_text[i] == j)
+      letter_value = (int)plain_text[i] - 32;
+    }
+    else
+    {
+      letter_value = (int)plain_text[i];
+    }
+
+    if (letter_value < 65 || letter_value > 91)
+    {
+      cipher_text[i] = plain_text[i];
+    }
+    else
+    {
+      for (int j = 65; j < 91; j++)
       {
-        cipher_text[i] = (char)values[j - 65];
+        if (letter_value == j)
+        {
+          if (is_lower)
+          {
+            cipher_text[i] = (char)(values[j - 65] + 32);
+          }
+          else
+          {
+            cipher_text[i] = (char)values[j - 65];
+          }
+        }
       }
     }
   }
