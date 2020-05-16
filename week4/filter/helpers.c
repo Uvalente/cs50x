430ff8224e07098f6b5f9acbfbcdac048eed91ff
Umberto Valente <uvalente89@gmail.com>
2020-05-16 19:55:34 +0100

    week4-filter: add greyscale filter

--- week4/filter/helpers.c
+++ week4/filter/helpers.c
@@ -1,8 +1,20 @@
 #include "helpers.h"
+#include <stdio.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            unsigned char average_color = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
+
+            image[i][j].rgbtBlue = average_color;
+            image[i][j].rgbtGreen = average_color;
+            image[i][j].rgbtRed = average_color;
+        }
+    }
     return;
 }
 
