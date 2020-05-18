18fff21958c7f7ecdc71bc7ce12ef578a4b24e7d
Umberto Valente <uvalente89@gmail.com>
2020-05-18 09:18:55 +0100

    week4-filter: add blur filter

--- week4/filter/helpers.c
+++ week4/filter/helpers.c
@@ -37,6 +37,35 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width - 1; j++)
+        {
+            long average_blue = 0x00;
+            long average_red = 0x00;
+            long average_green = 0x00;
+            for (int k = -1; k < 2; k++)
+            {
+                for (int l = -1; l < 2; l++)
+                {
+                    if ((i == 0 || j == 0) && (k == -1 || l == -1))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        average_blue += image[i + k][j + l].rgbtBlue;
+                        average_red += image[i + k][j + l].rgbtRed;
+                        average_green += image[i + k][j + l].rgbtGreen;
+                    }                    
+                }
+
+            }
+            image[i][j].rgbtBlue = average_blue / 9;
+            image[i][j].rgbtRed = average_red / 9;
+            image[i][j].rgbtGreen = average_green / 9;
+        }
+    }
     return;
 }
 
