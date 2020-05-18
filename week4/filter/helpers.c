bf68f5c2981bb4af0802dd1b8a18c0071383622a
Umberto Valente <uvalente89@gmail.com>
2020-05-18 14:18:53 +0100

    week4-filter: add black px value for out of border px to edge

--- week4/filter/helpers.c
+++ week4/filter/helpers.c
@@ -65,14 +65,14 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                     }
                     else
                     {
-                        divisor++;
                         average_blue += copy_image[i + k][j + l].rgbtBlue;
                         average_red += copy_image[i + k][j + l].rgbtRed;
                         average_green += copy_image[i + k][j + l].rgbtGreen;
-                    }                    
+                        divisor++;
+                    }
                 }
-
             }
+
             image[i][j].rgbtBlue = round(average_blue / divisor);
             image[i][j].rgbtRed = round(average_red / divisor);
             image[i][j].rgbtGreen = round(average_green / divisor);
@@ -84,7 +84,10 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    int Gx[9] = {-1, 0, 1, -2, 0, 2, -1, 0, 1};
+    int Gy[9] = {-1, -2, -1, 0, 0, 0, 1, 2, 1};
     RGBTRIPLE copy_image[height][width];
+
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
@@ -93,36 +96,36 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
         }
     }
 
-    int Gx[9] = {-1, 0, 1, -2, 0, 2, -1, 0, 1};
-    int Gy[9] = {-1, -2, -1, 0, 0, 0, 1, 2, 1};
-    int pixel_count = 0;
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width - 1; j++)
+        for (int j = 0; j < width; j++)
         {
-            long long orizontal_edge_blue = 0x00;
-            long long vertical_edge_blue = 0x00;
-            long long orizontal_edge_red = 0x00;
-            long long vertical_edge_red = 0x00;
-            long long orizontal_edge_green = 0x00;
-            long long vertical_edge_green = 0x00;
+            float orizontal_edge_blue = 0x00;
+            float vertical_edge_blue = 0x00;
+            float orizontal_edge_red = 0x00;
+            float vertical_edge_red = 0x00;
+            float orizontal_edge_green = 0x00;
+            float vertical_edge_green = 0x00;
             int matrix_count = 0;
 
             for (int k = -1; k < 2; k++)
             {
                 for (int l = -1; l < 2; l++)
                 {
-
-                    
-
-                    if ((i == 0 || j == 0) && (k == -1 || l == -1))
+                    if ((i == 0 && k == -1) || (j == 0 && l == -1) || (j == width - 1 && l == 1) || (i == height - 1 && k == 1))
                     {
-                        continue;
+                        orizontal_edge_blue += 0x00 * Gx[matrix_count];
+                        vertical_edge_blue += 0x00 * Gy[matrix_count];
+
+                        orizontal_edge_red += 0x00 * Gx[matrix_count];
+                        vertical_edge_red += 0x00 * Gy[matrix_count];
+
+                        orizontal_edge_green += 0x00 * Gx[matrix_count];
+                        vertical_edge_green += 0x00 * Gy[matrix_count];
                     }
                     else
                     {
                         orizontal_edge_blue += copy_image[i + k][j + l].rgbtBlue * Gx[matrix_count];
-
                         vertical_edge_blue += copy_image[i + k][j + l].rgbtBlue * Gy[matrix_count];
 
                         orizontal_edge_red += copy_image[i + k][j + l].rgbtRed * Gx[matrix_count];
@@ -130,37 +133,18 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
 
                         orizontal_edge_green += copy_image[i + k][j + l].rgbtGreen * Gx[matrix_count];
                         vertical_edge_green += copy_image[i + k][j + l].rgbtGreen * Gy[matrix_count];
-
-                        matrix_count++;
-                    }                    
+                    }
+                    matrix_count++;
                 }
-
-            }
-            long long blue = sqrt((orizontal_edge_blue * orizontal_edge_blue) + (vertical_edge_blue * vertical_edge_blue));
-
-            long long red = sqrt((orizontal_edge_red * orizontal_edge_red) + (vertical_edge_red * vertical_edge_red));
-
-            long long green = sqrt((orizontal_edge_green * orizontal_edge_green) + (vertical_edge_green * vertical_edge_green));
-
-            if (blue > 255)
-            {
-                blue = 255;
             }
 
-            if (red > 255)
-            {
-                red = 255;
-            }
-            if (green > 255)
-            {
-                green = 255;
-            }
+            int blue = round(sqrt((orizontal_edge_blue * orizontal_edge_blue) + (vertical_edge_blue * vertical_edge_blue)));
+            int red = round(sqrt((orizontal_edge_red * orizontal_edge_red) + (vertical_edge_red * vertical_edge_red)));
+            int green = round(sqrt((orizontal_edge_green * orizontal_edge_green) + (vertical_edge_green * vertical_edge_green)));
 
-            image[i][j].rgbtBlue = blue;
-
-            image[i][j].rgbtRed = red;
-
-            image[i][j].rgbtGreen = green;
+            image[i][j].rgbtBlue = blue > 255 ? 255 : blue;
+            image[i][j].rgbtRed = red > 255 ? 255 : red;
+            image[i][j].rgbtGreen = green > 255 ? 255 : green;
         }
     }
     return;
