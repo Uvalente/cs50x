275bc9d84af89a5a6a153a7250cc7cf98f5c08dd
Umberto Valente <uvalente89@gmail.com>
2020-05-20 04:34:54 +0100

    week4-recover: add jpg file creation

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -4,12 +4,14 @@
 
 int main(int argc, char *argv[])
 {
+  // Ensure proper usage
   if (argc == 1)
   {
     fprintf(stderr, "Usage: ./recover infile\n");
     return 1;
   }
 
+  // Open data file
   FILE *data_file = fopen(argv[1], "r");
   if (data_file == NULL)
   {
@@ -18,18 +20,39 @@ int main(int argc, char *argv[])
   }
 
   typedef uint8_t BYTE;
-
   BYTE data_buffer[512];
   int image_count = 0;
   char file_name[8];
+  FILE *recovered_data = NULL;
+  //bool open?
 
-  while(fread(&data_buffer, sizeof(BYTE), 512, data_file) == 512)
+  // Read data in 512 bytes chunks
+  while (fread(&data_buffer, sizeof(BYTE), 512, data_file) == 512)
   {
-    if (data_buffer[0] == 0xff && data_buffer[1] == 0xd8 && data_buffer[2] == 0xff && (data_buffer[3] & 0xf0) == 0xe0 )
+    // Check for jpg signature bytes
+    if (data_buffer[0] == 0xff && data_buffer[1] == 0xd8 && data_buffer[2] == 0xff && (data_buffer[3] & 0xf0) == 0xe0)
     {
+      // Close previous file if present
+      if (image_count > 0)
+      {
+        fclose(recovered_data);
+      }
+
+      // Create file name according to image count
       sprintf(file_name, "%03i.jpg", image_count);
-      printf("found image %s\n", file_name);
       image_count++;
+
+      // Open new file to write data
+      recovered_data = fopen(file_name, "w");
+      if (recovered_data == NULL)
+      {
+        fclose(data_file);
+        printf("Could not create 0");
+        return 3;
+      }
+
+      // Write data
+      fwrite(data_buffer, sizeof(BYTE), 512, recovered_data);
     }
   }
 
