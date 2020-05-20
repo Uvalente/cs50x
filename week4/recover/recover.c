d4165e6ff4e7032f3862e68812c3fa4ed90e1a62
Umberto Valente <uvalente89@gmail.com>
2020-05-20 03:18:20 +0100

    week4-recover: add error if file is not provided

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -3,5 +3,18 @@
 
 int main(int argc, char *argv[])
 {
+  if (argc == 1)
+  {
+    printf("Usage: ./recover infile\n");
+    return 1;
+  }
 
+  FILE *data_file = fopen(argv[1], "r");
+  if (data_file == NULL)
+  {
+    printf("Could not open %s\n", argv[1]);
+    return 2;
+  }
+
+  return 0;
 }
