1a23a306dc304e87a6b8a866f1184ab7161cce12
Umberto Valente <uvalente89@gmail.com>
2020-05-24 23:49:38 +0100

    week6-readability: add grade calculation

--- week6/readability/readability.py
+++ week6/readability/readability.py
@@ -4,13 +4,21 @@ letters = 0
 sentences = 0
 
 for word in text.split():
-  words += 1
-  for letter in word:
-    if letter.isalpha():
-      letters += 1
-    elif letter in [".", "!", "?"]:
-      sentences += 1
+    words += 1
+    for letter in word:
+        if letter.isalpha():
+            letters += 1
+        elif letter in [".", "!", "?"]:
+            sentences += 1
 
-print(letters)
-print(words)
-print(sentences)
+avg_letters = (letters * 100) / words
+avg_sentences = (sentences * 100) / words
+
+grade = round(0.0588 * avg_letters - 0.296 * avg_sentences - 15.8)
+
+if grade < 1:
+    print("Before Grade 1")
+elif grade >= 16:
+    print("Grade 16+")
+else:
+    print(f"Grade {grade}")
