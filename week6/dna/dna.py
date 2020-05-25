57d55c9ef1062e699be0865e872f590e71d3695e
Umberto Valente <uvalente89@gmail.com>
2020-05-25 23:42:53 +0100

    week6-dna: add sequence counting method

--- week6/dna/dna.py
+++ week6/dna/dna.py
@@ -15,5 +15,20 @@ def main():
             del row["name"]
             dataset[name] = row
 
-   
+
+def counting(text, word):
+    max_count = 0
+    word_length = len(word)
+
+    for i in range(len(text)):
+        current_count = 0
+        while text[i:i + word_length] == word:
+            current_count += 1
+            i += word_length
+        if current_count > max_count:
+            max_count = current_count
+
+    return max_count
+
+
 main()
