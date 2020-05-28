adf978814068f81ecd9366dc3abb8a5537cd4548
Umberto Valente <uvalente89@gmail.com>
2020-05-28 14:07:13 +0100

    week7-movies: add 10.sql query

--- week7/movies/5.sql
+++ week7/movies/5.sql
@@ -1,1 +1,3 @@
-SELECT title, year FROM movies WHERE title LIKE 'harry potter%' ORDER BY year;
+SELECT title, year FROM movies
+WHERE title LIKE 'harry potter%'
+ORDER BY year;
--- week7/movies/7.sql
+++ week7/movies/7.sql
@@ -1,1 +1,4 @@
-SELECT title, ratings.rating FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE year = 2010 ORDER BY ratings.rating DESC, title;
+SELECT title, ratings.rating FROM movies
+JOIN ratings ON movies.id = ratings.movie_id
+WHERE year = 2010
+ORDER BY ratings.rating DESC, title;
