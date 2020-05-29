a024a9b81b0b312a95943febb7f4768c4ed18795
Umberto Valente <uvalente89@gmail.com>
2020-05-29 09:51:52 +0100

    week7-houses: add db query and student list printing

--- week7/houses/roster.py
+++ week7/houses/roster.py
@@ -1,1 +1,16 @@
-# TODO
+from sys import argv, exit
+import sqlite3
+
+if not len(argv) == 2:
+    print("Usage: python roster.py houseName")
+    exit(1)
+
+connection = sqlite3.connect("students.db")
+db = connection.cursor()
+
+for row in db.execute("SELECT * FROM students WHERE house = ? ORDER BY last, first;", (argv[1],)):
+    print(row[1], row[2], row[3], end=", ") if row[2] else print(
+        row[1], row[3], end=', ')
+    print("born", row[5])
+
+connection.close()
